d497d3860a23648e8d1a7298b5f411c92fe45cca
jongsik22 <xxx_@kakao.com>
2023-09-04 18:09:43 +0900

    ADD: Subtract.java

---
+++ Subtract.java
@@ -0,0 +1,11 @@
+public class Subtract {
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        System.out.println(subtract(a, b));
+    }
+}
